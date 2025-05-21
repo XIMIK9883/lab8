5675cc2045eafc006dbd1b146460cdd6a16dc29b
XIMIK9883 <fernandoivan944@gmail.com>
2025-05-21 12:28:11 +0300

    "Добавлен вывод общей информации о задании"

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #0. Library Subscription\n";
-    cout << "Author: Sergey Yermochenko\n";
+    cout << "Variant #4. Library Subscription\n";
+    cout << "Author: Zaitsev Ivan\n";
     return 0;
 }
